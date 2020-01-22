292f3ed654d82df1d84a84427549c691e56cb111
Nicholas Van den Berghe <nicholasvdberghe@hotmail.com>
2020-01-22 19:00:56 +0100

    validatieboodschappen

--- KaartingenGrootDeinze/Models/ContactViewModel.cs
+++ KaartingenGrootDeinze/Models/ContactViewModel.cs
@@ -9,6 +9,7 @@ namespace KaartingenGrootDeinze.Models
         public string Afzender { get; set; }
         [Required(ErrorMessage = "{0} is verplicht")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Geef een geldig e-mailadres op")]
         public string Emailadres { get; set; }
         [Required(ErrorMessage = "{0} is verplicht")]
         public string Onderwerp { get; set; }
